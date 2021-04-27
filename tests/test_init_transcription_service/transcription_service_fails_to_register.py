8e20d01b5339e93d01b5a6b5b8886200f8711826
larry kirschner <larrykirschner@gmail.com>
2021-04-27 16:29:02 -0700

    fixes typo

--- tests/test_init_transcription_service/transcription_service_fails_to_register.py
+++ tests/test_init_transcription_service/transcription_service_fails_to_register.py
@@ -1,5 +1,5 @@
 import logging
 
 logging.warning(
-    "this module does nothing. It exists to test a loadale module that SHOULD register a factory for a TranscriptionService but fails to do so"
+    "this module does nothing. It exists to test a loadable module that SHOULD register a factory for a TranscriptionService but fails to do so"
 )
